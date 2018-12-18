b2793e4acea590ee40ce0a31df0d0e5761f72e13
Tom Settle <tsettle44@gmail.com>
2018-12-17 21:40:25 -0500

    Posts and comment API

--- models/post.js
+++ models/post.js
@@ -1,5 +1,56 @@
 var mongooose = require('mongoose');
 
+var CommentToCommentSchema = new mongooose.Schema({
+    postedBy: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now()
+    },
+  })
+
+var CommentSchema = new mongooose.Schema({
+    postedBy: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now()
+    },
+    likes: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    comments: [CommentToCommentSchema]
+});
+
+CommentSchema.method("like", function(vote, callback){
+  this.likes += 1;
+  this.parent().save(callback);
+});
+
 var PostSchema = new mongooose.Schema({
     postedBy: {
         type: String,
@@ -22,8 +73,14 @@ var PostSchema = new mongooose.Schema({
           type: Number,
           required: true,
           default: 0
-      }
+      },
+      comments: [CommentSchema] 
 });
 
-var Post = mongooose.model('Post' , PostSchema);
-module.exports = Post;
+PostSchema.method("like", function(vote, callback){
+  this.likes += 1;
+  this.save(callback);
+});
+
+var Post = mongooose.model('Post', PostSchema);
+module.exports.Post = Post;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
+var Post = require('../models/post').Post;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -111,5 +112,99 @@ router.get('/profile', function( req, res, next) {
 router.get('/about', function(req, res, next) {
   res.render('about', { title: "About"});
 });
+
+//GET Discussion Board
+router.get('/discussion-board', function(req, res, next) {
+  res.render('discussion', { title: "Discussion Board"});
+});
+
+//Discussion Board Post Param
+router.param("pID", function(req, res, next, id) {
+  Post.findById(req.params.pID, function(err, doc){
+      if(err) return next(err);
+      if(!doc) {
+          err = new Error("Not Found");
+          err.status = 404;
+          return next(err);
+      }
+      req.post = doc;
+      return next();
+  });
+});
+
+//Discussion Board Comment Param
+router.param("cID", function(req, res, next, id) {
+  req.comment = req.post.comments.id(id);
+  if(!req.comment) {
+      err = new Error("Not Found");
+      err.status = 404;
+      return next(err);
+  }
+  next();
+});
+
+//GET Discussion Board POSTS
+router.get("/discussion-board/posts", function(req, res, next){
+  Post.find({})
+      .sort({createdAt: -1})
+      .exec(function(err, posts) {
+      if(err) return next(err);
+      res.json(posts);
+  });
+});
+
+//GET Specific Post
+router.get("/discussion-board/posts/:pID", function(req, res){
+  res.json(req.post);
+});
+
+//POST Post on Discussion Board
+router.post("/discussion-board/posts", function(req, res, next){
+  var post = new Post(req.body);
+  post.save(function(err, post) {
+      if(err) return next(err);
+      res.status(201);
+      res.json(post);
+  });
+});
+
+//POST Like Post
+router.post("/discussion-board/posts/:pID/like", 
+    function(req, res, next){
+        req.post.like(req.like, function(err, post) {
+            if(err) return next(err);
+            res.json(post);
+        });
+});
+
+//POST Comment to Post
+router.post("/discussion-board/posts/:pID/comments", function(req, res, next){
+  req.post.comments.push(req.body);
+  req.post.save(function(err, post){
+      if(err) return next(err);
+      res.status(201);
+      res.json(post);
+  });
+});
+
+//POST Comment to Comment
+router.post("/discussion-board/posts/:pID/:cID/comments", function(req, res, next){
+  req.comment.comments.push(req.body);
+  req.post.save(function(err, comment){
+      if(err) return next(err);
+      res.status(201);
+      res.json(comment);
+  });
+});
+
+//POST Like comment
+router.post("/discussion-board/posts/:pID/:cID/like", 
+    function(req, res, next){
+        req.comment.like(req.like, function(err, post) {
+            if(err) return next(err);
+            res.json(post);
+        });
+});
+
 
 module.exports = router;
